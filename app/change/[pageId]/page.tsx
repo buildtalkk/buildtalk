81a4a6994f191855c3fce1807cd17f6b15eee3a5
이동섭 <ds4702@gmail.com>
2024-08-17 16:05:50 +0900

    build 에러 수정

--- app/change/[pageId]/page.tsx
+++ app/change/[pageId]/page.tsx
@@ -1,15 +1,12 @@
+import { Footer } from "@/components/Footer";
+import { Navbar } from "@/components/Navbar";
+import { ScrollToTop } from "@/components/ScrollToTop";
+import NotionPage from "@/components/notion/notionPages";
+import { ThemeProvider } from "@/components/theme-provider";
+import { NotionAPI } from "notion-client";
+import { Suspense } from "react";
 
-import { Footer } from '@/components/Footer';
-import { Navbar } from '@/components/Navbar';
-import { ScrollToTop } from '@/components/ScrollToTop';
-import NotionPage from '@/components/notion/notionPages';
-import { ThemeProvider } from '@/components/theme-provider';
-import { NotionAPI } from 'notion-client';
-import { Suspense } from 'react';
-
-
-export const notion = new NotionAPI();
-
+const notion = new NotionAPI();
 
 const Home = async ({ params }: { params: { pageId: string } }) => {
   if (params.pageId) {
@@ -17,21 +14,19 @@ const Home = async ({ params }: { params: { pageId: string } }) => {
       const recordMap = await notion.getPage(params.pageId); // 2. 최상단 페이지 데이터 가져오기
 
       return (
-         <ThemeProvider>
-      <Navbar />
-      <Suspense>
-      <NotionPage recordMap={recordMap} />
-      </Suspense>
-      <Footer />
-      <ScrollToTop />
-    </ThemeProvider>
-
-
+        <ThemeProvider>
+          <Navbar />
+          <Suspense>
+            <NotionPage recordMap={recordMap} />
+          </Suspense>
+          <Footer />
+          <ScrollToTop />
+        </ThemeProvider>
       );
     } catch (error) {
       return console.error(error);
     }
-  } else return <div>error</div>
+  } else return <div>error</div>;
 };
 
 export default Home;
--- app/change/page.tsx
+++ app/change/page.tsx
@@ -1,16 +1,13 @@
-import { Footer } from '@/components/Footer';
-import { Navbar } from '@/components/Navbar';
-import { ScrollToTop } from '@/components/ScrollToTop';
-import NotionPage from '@/components/notion/notionPages';
-import { ThemeProvider } from '@/components/theme-provider';
-import { NotionAPI } from 'notion-client';
-import { Suspense } from 'react';
+import { Footer } from "@/components/Footer";
+import { Navbar } from "@/components/Navbar";
+import { ScrollToTop } from "@/components/ScrollToTop";
+import NotionPage from "@/components/notion/notionPages";
+import { ThemeProvider } from "@/components/theme-provider";
+import { NotionAPI } from "notion-client";
+import { Suspense } from "react";
 
-
-const pageId = 'a1ebd71e01404b13aea9bdd7efb91271';
-export const notion = new NotionAPI();
-
-
+const pageId = "a1ebd71e01404b13aea9bdd7efb91271";
+const notion = new NotionAPI();
 
 const Home = async () => {
   if (pageId) {
@@ -18,20 +15,19 @@ const Home = async () => {
       const recordMap = await notion.getPage(pageId); // 2. 최상단 페이지 데이터 가져오기
 
       return (
-           <ThemeProvider>
-      <Navbar />
-      <Suspense>
-      <NotionPage recordMap={recordMap} />
-      </Suspense>
-      <Footer />
-      <ScrollToTop />
-    </ThemeProvider>
-
+        <ThemeProvider>
+          <Navbar />
+          <Suspense>
+            <NotionPage recordMap={recordMap} />
+          </Suspense>
+          <Footer />
+          <ScrollToTop />
+        </ThemeProvider>
       );
     } catch (error) {
       return console.error(error);
     }
-  } else return <div>error</div>
+  } else return <div>error</div>;
 };
 
 export default Home;
