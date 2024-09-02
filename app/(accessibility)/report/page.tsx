44fce1af072363a024c6ef4bfc60be734ed84fbf
humonnom <juepark42@gmail.com>
2024-09-02 16:50:13 +0900

    report page 뼈대 작업

--- app/(accessibility)/report/page.tsx
+++ app/(accessibility)/report/page.tsx
@@ -1,5 +1,24 @@
+"use client";
+import useSessionStorageState from "use-session-storage-state";
+import { SelectedInfo } from "@/types";
+
 const ReportPage = () => {
-  return <p>분석 결과</p>;
+  const [selectedInfo] = useSessionStorageState<SelectedInfo | undefined>(
+    "selectedInfo"
+  );
+
+  if (!selectedInfo) {
+    return <div>분석 결과가 없습니다.</div>;
+  }
+
+  return (
+    <div>
+      <p>분석 결과</p>
+      <p>{selectedInfo.mainCategory}</p>
+      <p>{selectedInfo.subCategory}</p>
+      <p>{selectedInfo.area}</p>
+    </div>
+  );
 };
 
 export default ReportPage;
